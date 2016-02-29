2bd9e0a01893cd3ea86ef562a5d0933e86f316ab
Ben Ronda and Conner Belvin <c9er99@gmail.com>
2016-02-29 15:57:43 -0800

    add all passing tests and a one-to-many relationship

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -5,6 +5,7 @@
     */
 
     require_once "src/Category.php";
+    require_once "src/Task.php";
 
     $server = 'mysql:host=localhost;dbname=to_do_test';
     $username = 'root';
@@ -14,6 +15,7 @@
     class CategoryTest extends PHPUnit_Framework_TestCase{
         protected function tearDown(){
             Category::deleteAll();
+            Task::deleteAll();
         }
 
         function test_getName(){
@@ -82,6 +84,27 @@
             $result = Category::find($test_category->getId());
 
             $this->assertEquals($test_category, $result);
+        }
+
+        function test_getTasks(){
+          $name = "Work Stuff";
+          $id = null;
+          $test_category = new Category($name, $id);
+          $test_category->save();
+
+          $test_category_id = $test_category->getId();
+
+          $description = "Email client";
+          $test_task = new Task($description, $id, $test_category_id);
+          $test_task->save();
+
+          $description2 = "Meet with boss";
+          $test_task2 = new Task($description2, $id, $test_category_id);
+          $test_task2->save();
+
+          $result = $test_category->getTasks();
+
+          $this->assertEquals([$test_task, $test_task2], $result);
         }
     }
 ?>
